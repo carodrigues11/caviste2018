829e3ee306bae59dd43fbb01146c574b00850ed3
RODRIGUES GONZALEZ CAROLINE <1112carodriguesgonza@domepfc.net>
2018-02-24 13:35:41 +0100

    Définition des routes de l'API

---
+++ server/src/routes.php
@@ -0,0 +1,54 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use \RedBeanPHP\R as R;
+
+
+// Routes
+/*
+$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+*/
+
+$app->get('/api/', function (Request $request, Response $response, array $args) {
+    echo "API!";
+});
+
+$app->get('/api/wines/', function (Request $request, Response $response, array $args) {
+    $wines = R::findAll('wine');
+    return json_encode($wines,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+});
+
+$app->get('/api/search/{name}', function (Request $request, Response $response, array $args) {
+    $name = R::load('wine', '');
+    echo $name;
+});
+
+$app->get('/api/wines/{id}', function (Request $request, Response $response, array $args) {
+    $wine = R::load('wine', $id);
+    echo $wine;
+});
+
+$app->post('/api/wines', function (Request $request, Response $response, array $args) {
+    $newWine = R::dispense('wine');
+    echo $newWine;
+});
+
+$app->put('/api/wines/$id', function (Request $request, Response $response, array $args) {
+    echo "Modifie les données du vin id ==10";
+});
+
+$app->delete('/api/wines/7', function (Request $request, Response $response, array $args) {
+    $wine = R::trash('wine', 7);
+    echo $wine;
+});
+
+$app->get('/catalogue/', function (Request $request, Response $response, array $args) {
+    echo "catalogue!";
+});
